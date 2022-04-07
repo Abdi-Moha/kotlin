479ee279a72a39b1efedbdba6b54b18b8c876cb7
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-04-07 15:32:03 +0200

    feat: add limited support of comments inside js call.

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/BoxJsTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/BoxJsTestGenerated.java
@@ -6121,6 +6121,12 @@ public class BoxJsTestGenerated extends AbstractBoxJsTest {
         }
 
         @Test
+        @TestMetadata("comments.kt")
+        public void testComments() throws Exception {
+            runTest("js/js.translator/testData/box/jsCode/comments.kt");
+        }
+
+        @Test
         @TestMetadata("constantExpression.kt")
         public void testConstantExpression() throws Exception {
             runTest("js/js.translator/testData/box/jsCode/constantExpression.kt");
